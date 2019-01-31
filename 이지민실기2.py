35c4aadbc93687d4b74d9795dc6d9135c5ff2786
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2019-01-31 16:18:47 +0900

    Update 이지민실기2.py

--- 이지민실기2.py
+++ 이지민실기2.py
@@ -1,7 +1,11 @@
+# def check_id(message):
+#     for i in message:
+#         if i != 0 or  i != 1 or i != 2 or i != 3 or i != 4 or i !=5 or i != 6 or i != 7 or i != 8 or i != 9:
+#             print("아이디는 문자와 숫자가 같이 포함되어야 합니다")
+
 def check_id(message):
-    for i in message:
-        if i != 0 or  i != 1 or i != 2 or i != 3 or i != 4 or i !=5 or i != 6 or i != 7 or i != 8 or i != 9:
-            print("아이디는 문자와 숫자가 같이 포함되어야 합니다")
+	return not(message.isalpha() or message.isdigit())
+
 
 def generate_password():
     import random
@@ -27,12 +31,17 @@ def display_member(member):
         print("비밀번호 : ", generate_password())
         print("추가정보 : ", input_addinfo(weight_a, height_a))
 
-    
+
 d = []
 
 for i in range(2):
     lst = []
-    user_id = input("아이디 입력 : ")
+    while True:
+        user_id = input("아이디 입력 : ")
+        if check_id(user_id):
+            break
+        else:
+            print("아이디는 문자와 숫자가 같이 포함되어야 합니다")
     lst.append(user_id)
     height = int(input("신장 입력 : "))
     lst.append(height)
