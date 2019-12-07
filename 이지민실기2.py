6c90a1e59704bfd329ae1ea4b10ee6ddc0113b9f
Yoon Dae Heon <44860482+Greathoney@users.noreply.github.com>
2019-12-07 22:53:11 +0900

    Update 이지민실기2.py

--- 이지민실기2.py
+++ 이지민실기2.py
@@ -1,33 +1,18 @@
 def check_id(message):
-    for i in message:
-        if i != 0 or  i != 1 or i != 2 or i != 3 or i != 4 or i !=5 or i != 6 or i != 7 or i != 8 or i != 9:
-            print("아이디는 문자와 숫자가 같이 포함되어야 합니다")
+    if message.isdigit() or message.isalpha(): # if <message> only digit or alphabet
+        print("아이디는 문자와 숫자가 같이 포함되어야 합니다")
 
 def generate_password():
     import random
-    alp = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    a = random.choice(alp)
-    b = random.choice(alp)
-    c = random.choice(alp)
-    d = random.choice(alp)
-    e = random.choice(alp)
-    f = random.choice(alp)
-    return (a+b+c+d+e+f)
-
-
-def input_addinfo(info1, info2):
-    return [info1, info2]
-
+    return ''.join([chr(random.randint(ord('a'), ord('z'))) for i in range(6)])
 
 def display_member(member):
     for j in range(2):
-        weight_a = d[j][1]
-        height_a = d[j][2]
+        print()
         print("아이디 : ", d[j][0])
         print("비밀번호 : ", generate_password())
-        print("추가정보 : ", input_addinfo(weight_a, height_a))
+        print("추가정보 : ", d[j][1], d[j][2]) # (weight, height)
 
-    
 d = []
 
 for i in range(2):
